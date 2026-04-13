fb9fc34e4a712c129489d1739776a0750d37d442
Zero Y One K <markbillhv@gmail.com>
2026-04-13 16:31:02 +0700

    testing icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center bg-red-200">
+    <View className="flex-1 justify-center items-center">
       <Text className="text-2xl font-bold text-white">Home</Text>
+      <AntDesign name="caret-right" size={24} color="white" />
     </View>
   );
 }
