513d2e1964521c2ee1eb6de5201c0aaa16bc827a
Zero Y One K <markbillhv@gmail.com>
2026-04-13 16:21:12 +0700

    nativewind setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import "../../global.css"
 import { Stack } from "expo-router";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,8 @@
 import { View, Text } from "react-native";
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home</Text>
+    <View className="flex-1 justify-center items-center bg-red-200">
+      <Text className="text-2xl font-bold text-white">Home</Text>
     </View>
   );
 }
