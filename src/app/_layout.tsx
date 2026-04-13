de361bf9f1b60d1774d80000f8ffb98e5f8e3eee
Zero Y One K <markbillhv@gmail.com>
2026-04-13 15:47:42 +0700

    set up expo router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <Stack >
+        <Stack.Screen name="index"  />
+        </Stack>
+    </ThemeProvider>
+  );
+}
